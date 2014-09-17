3f07c616152c169709df8f057da7a18c1babab0a
ngh <ngh@nghdeiMac.local>
2014-09-17 12:57:03 -0700

    Initial Commit

---
+++ trial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  trial
+//
+//  Created by ngh on 14-9-17.
+//  Copyright (c) 2014年 ngh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ trial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  trial
+//
+//  Created by ngh on 14-9-17.
+//  Copyright (c) 2014年 ngh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
